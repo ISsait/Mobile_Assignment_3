45dc1b03444d666840d205bd235d43f89183e32c
ISsait <152314143+ISsait@users.noreply.github.com>
2024-10-15 18:05:35 -0600

    initial commit

---
+++ src/Main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+    View,
+    Text,
+} from "react-native";
+
+
+
+
+function Main() : React.JSX.Element {
+    return (
+        <View>
+            <Text>Main Component</Text>
+        </View>
+    );
+}
+    
+
+export default Main;
